e177dc1122824de893e2e5bd75df7ef109cb0cdb
Tyler Tuan Tran <87620828+ttran2001@users.noreply.github.com>
2022-10-25 13:39:56 -0500

    Add files via upload
    Signed-off-by: Tyler Tuan Tran <87620828+ttran2001@users.noreply.github.com>

---
+++ backend/Cutting_Algo.py
@@ -0,0 +1,25 @@
+import array as arr
+
+
+class Cutting_Algo:
+
+    def __init__(self, width, length):
+        self.width = width
+        self.length = length
+
+    def driveInstructions(array):
+        for x in width:
+            for y in length:
+
+    def getLength():
+        return length
+
+    def getWidth():
+        return width
+
+    def __main():
+        Cutting_Algo()
+
+
+width = 0
+length = 0
---
+++ backend/Driver.py
@@ -0,0 +1,15 @@
+class Driver:
+    def turnRight():
+        return null
+
+    def turnLeft():
+        return null
+
+    def rotateWheels():
+        return null
+
+    def brake():
+        return null
+
+    def main():
+        return null
