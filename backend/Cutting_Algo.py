9300fce364e586dc83828400e08fca623d37427d
Tyler Tuan Tran <87620828+ttran2001@users.noreply.github.com>
2022-10-28 00:44:23 -0500

    Update Cutting_Algo.py
    Signed-off-by: Tyler Tuan Tran <87620828+ttran2001@users.noreply.github.com>

--- backend/Cutting_Algo.py
+++ backend/Cutting_Algo.py
@@ -2,24 +2,52 @@ import array as arr
 
 
 class Cutting_Algo:
-
-    def __init__(self, width, length):
+    def __init__(self, width, length, array):
         self.width = width
         self.length = length
+        self.endpoint = False
+        self.grassDesign = array
+        self.val = 0
+        self.cut = False
 
-    def driveInstructions(array):
-        for x in width:
-            for y in length:
+    def driveInstructions(self):
+        for row in self.length:
+            if (self.endpoint == False):
+                for column in reversed(self.width):
+                    val = self.grassDesign[row][column]
+                    if (val == 0):
+                        self.cut = False
+                    else:
+                        self.cut = True
+                # call mower
+                # rotateWheels()
+                # brake()
+            else:
+                for column in self.width:
+                    # move
+                    val = self.grassDesign[row][column]
+                    if (val == 0):
+                        self.cut = False
+                    else:
+                        self.cut = True
+                # call mower
+                # rotateWheels
+                # brake()
+            if (self.endpoint == True):
+                self.cut = False
+                # turnRight()
+                # rotateWheels()
+                # brake()
+                # turnRight()
+            else:
+                self.cut = True
+                # turnLeft()
+                # rotateWheels()
+                # brake()
+                # turnLeft()
 
-    def getLength():
-        return length
+    def findLengthOfFence(self):
+        return self.length
 
-    def getWidth():
-        return width
-
-    def __main():
-        Cutting_Algo()
-
-
-width = 0
-length = 0
+    def findWidthOfFence(self):
+        return self.width
