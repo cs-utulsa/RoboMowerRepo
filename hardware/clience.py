cf3bbbb460569bef7e22715622da9833072312d9
Tyler Tuan Tran <87620828+ttran2001@users.noreply.github.com>
2022-11-02 23:24:29 -0500

    Update clience.py
    Signed-off-by: Tyler Tuan Tran <87620828+ttran2001@users.noreply.github.com>

--- hardware/clience.py
+++ hardware/clience.py
@@ -12,7 +12,7 @@ class ClientSocketCar(object):
         initializing network socket
         '''
         self.clinet_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create Network socket
-        self.clinet_socket.connect(('172.16.194.57', 9900))  # ip address ,port number
+        self.clinet_socket.connect(('172.16.195.11', 22))  # ip address ,port number
 
     # sending data
     def up_socket(self):
