51d63de3ebe6b129d7bffa64c91d241e97b9d4cc
Jeff Luong <56270844+JeffL2019@users.noreply.github.com>
2022-11-21 01:23:59 -0600

    Add files via upload
    Signed-off-by: Jeff Luong <56270844+JeffL2019@users.noreply.github.com>

--- backend/testworking.py
+++ backend/testworking.py
@@ -3,14 +3,15 @@ import time
 from gpiozero import Robot
 from gpiozero.pins.pigpio import PiGPIOFactory
 
-factory = PiGPIOFactory(host='192.168.1.32')
+factory = PiGPIOFactory(host='192.168.1.32') #change ip to what the ip is for the RoboMower
 
 robot = Robot(left=(23, 22), right=(24, 25), pin_factory=factory)
 
 
-
+#runs the robot forward at max speed 1 for 1 second
 robot.forward(1)
 time.sleep(1)
+#runs the robot backwards at max speed 1 for 1 second
 robot.backward(1)
 time.sleep(1)
 robot.stop()
