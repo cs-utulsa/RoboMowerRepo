4539f7a37993f170bb064127a1d37e43b38afac6
Tyler Tuan Tran <87620828+ttran2001@users.noreply.github.com>
2023-03-05 12:03:35 -0600

    Update TurtleToGPIO.py
    Signed-off-by: Tyler Tuan Tran <87620828+ttran2001@users.noreply.github.com>

--- backend/TurtleToGPIO.py
+++ backend/TurtleToGPIO.py
@@ -1,8 +1,7 @@
 import RPi.GPIO as GPIO
 import time
+import PNGtoArray
 import turtle
-import Draw
-
 
 # Define the pins connected to the left motor
 left_motor_forward_pin = 11
@@ -19,6 +18,13 @@ GPIO.setup(left_motor_backward_pin, GPIO.OUT)
 GPIO.setup(right_motor_forward_pin, GPIO.OUT)
 GPIO.setup(right_motor_backward_pin, GPIO.OUT)
 
+# Defining the pins for mower on and off by using LED method
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+ledPin = 12
+
+GPIO.setup(ledPin, GPIO.OUT)
 
 # Define the robot's movements
 def forward():
@@ -55,51 +61,18 @@ def stop():
     GPIO.output(left_motor_backward_pin, GPIO.LOW)
     GPIO.output(right_motor_backward_pin, GPIO.LOW)
 
-# Define the movements to draw the pattern
-def onecycle():
-    forward()
-    for i in range(69):
-        time.sleep(0.01)
-        forward()
-
-    stop()
+def down():
+    GPIO.output(ledPin, GPIO.HIGH)
 
 
-def zigzag():
-    for i in range(29):
-        onecycle()
-        if i % 2 == 0:
-            if i != 28:
-                left()
-                time.sleep(0.2)
-                stop()
-                right()
-                time.sleep(0.2)
-                stop()
-        else:
-            right()
-            time.sleep(0.2)
-            stop()
-            left()
-            time.sleep(0.2)
-            stop()
+def up():
+    GPIO.output(ledPin, GPIO.LOW)
 
 
-binary_array = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
-    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
-    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
-    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
-
-numrows = len(binary_array)
-numcols = len(binary_array[0])
+binaryArt = PNGtoArray.image_to_array()
+# Initialize the turtle graphics window
+numrows = len(binaryArt)
+numcols = len(binaryArt[0])
 
 screen_width = numcols * 10
 screen_height = numrows * 10
@@ -117,102 +90,48 @@ a.color("green")
 
 rows = 0
 
+# Convert the turtle commands to robot movements
+def turtle_to_robot_forward(distance):
+    forward()
+    a.forward(distance)
+
+def turtle_to_robot_left(angle):
+    left()
+    a.left(angle)
+
+def turtle_to_robot_right(angle):
+    right()
+    a.right(angle)
+
+def turtle_to_robot_pendown():
+    down()
+    a.pendown()
+
+def turtle_to_robot_penup():
+    up()
+    a.penup()
+
+# Define the movements to draw the pattern
 def onecycle():
     global rows
     for i in range(numcols):
-        if binary_array[rows][i] == 1:
-            a.pendown()
-            a.forward(10)
+        if binaryArt[rows][i] == 1:
+            turtle_to_robot_pendown()
+            turtle_to_robot_forward(10)
         else:
-            a.penup()
-            a.forward(10)
+            turtle_to_robot_penup()
+            turtle_to_robot_forward(10)
     rows += 1
 
 for i in range(numrows):
     onecycle()
     if rows % 2 == 1:
-        a.right(90)
-        a.penup()
-        a.forward(10)
-        a.right(90)
+        turtle_to_robot_right(90)
+        turtle_to_robot_penup()
+        turtle_to_robot_forward(10)
+        turtle_to_robot_right(90)
     else:
-        a.left(90)
-        a.penup()
-        a.forward(10)
-        a.left(90)
-
-turtle.exitonclick()
-
-
-'''
-
-# Initialize the turtle graphics window
-binary_array = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
-    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
-    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
-    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
-
-numrows = len(binary_array)
-numcols = len(binary_array[0])
-
-screen_width = numcols * 10
-screen_height = numrows * 10
-
-turtle.setup(width=screen_width, height=screen_height)
-turtle.bgcolor("white")
-
-a = turtle.Turtle()
-a.penup()
-a.goto(-screen_width/2, screen_height/2)
-a.pendown()
-a.speed(10)
-a.pensize(3)
-a.color("green")
-rows = 0
-
-# Convert the turtle commands to robot movements
-def turtle_to_robot_forward():
-    forward()
-    a.forward(10)
-
-def turtle_to_robot_left():
-    left()
-    a.left(90)
-
-def turtle_to_robot_right():
-    right()
-    a.right(90)
-
-def onecycle():
-    global rows
-    for i in range(numcols):
-        if binary_array[rows][i] == 1:
-            a.pendown()
-            turtle_to_robot_forward()
-        else:
-            a.penup()
-            turtle_to_robot_forward()
-    rows += 1
-
-# Call the turtle commands and convert them to robot movements
-for i in range(numrows):
-    onecycle()
-    if rows % 2 == 0:
-        turtle_to_robot_left()
-        a.penup()
-        turtle_to_robot_forward()
-        turtle_to_robot_left()
-    else:
-        turtle_to_robot_right()
-        a.penup()
-        turtle_to_robot_forward()
-        turtle_to_robot_right()
-'''
+        turtle_to_robot_left(90)
+        turtle_to_robot_penup()
+        turtle_to_robot_forward(10)
+        turtle_to_robot_left(90)
